9736117eeea4063c13692d02dd29d3f183171216
Tsveta <tsvetadzhevelekova@gmail.com>
2025-05-22 19:43:07 +0300

    calculate the price of one item

--- cart.js
+++ cart.js
@@ -1,12 +1,19 @@
-/*Tsveta*/ function costItem(totalCost, count){
-    if(count === 0){
-        return 0;
-    }
-    const priceOne = totalCost / count;
-    return{
-        count: count,
-        priceOne: priceOne.toFixed(2)
+/*Tsveta*/ 
+    function addToCart() {
+    const itemPrice = 8.99;
+    const quant = parseInt(document.querySelector(".quantity-box").value);
+    const price = (itemPrice * quant).toFixed(2);
+    const item = {
+        name: " Lush Lemon",
+        quant: quant,
+        itemPrice: itemPrice,
+        price: price
     };
+    console.log(price);
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    cart.push(item);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    
 }
 
 
