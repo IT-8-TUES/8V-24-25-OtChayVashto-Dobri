00752669753342e60a739bedf164b469652dd489
Tsveta <tsvetadzhevelekova@gmail.com>
2025-05-23 12:51:41 +0300

    calculate the price of the items and add to cart with index next to the cart icon

--- cart.js
+++ cart.js
@@ -1,21 +1,32 @@
 /*Tsveta*/ 
-    function addToCart() {
+    function addToCart(button) {
+    const productEl = button.closest('.product-info');
+    const name = productEl.querySelector('.text p');
+    const quant = parseInt(productEl.querySelector(".quantity-box").value);
     const itemPrice = 8.99;
-    const quant = parseInt(document.querySelector(".quantity-box").value);
     const price = (itemPrice * quant).toFixed(2);
     const item = {
-        name: " Lush Lemon",
+        name: name,
         quant: quant,
         itemPrice: itemPrice,
         price: price
     };
-    console.log(price);
     let cart = JSON.parse(localStorage.getItem("cart")) || [];
     cart.push(item);
-    localStorage.setItem("cart", JSON.stringify(cart));
-    
+    localStorage.setItem("cart", JSON.stringify(cart)); 
+    cartQuant(); 
+    console.log(price);
 }
 
-
+function cartQuant(){
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    let total = 0;
+    cart.forEach(item => total += item.quant);
+    const carCountEl = document.getElementById("cart-count");
+    if(carCountEl){
+        carCountEl.textContent = total;
+    }
+}
+document.addEventListener("DOMContentLoaded", cartQuant);
 
 /*Tsveta*/
